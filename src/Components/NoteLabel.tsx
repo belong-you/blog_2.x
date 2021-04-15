6d73c242a6d22850f1c46236a9d2ee6a414f0a7c
belong-you <1781926993@qq.com>
2021-04-15 12:22:39 +0800

    添加延时加载

--- src/Components/NoteLabel.tsx
+++ src/Components/NoteLabel.tsx
@@ -1,16 +1,24 @@
 import { NavLink } from 'umi';
+import { sleep } from '@/utils/browser';
+import { useState } from 'react';
+const { ['log'] : c } = console;
 
 const NoteLabel = ({ list, path = '' }: any) => {
+  const [ flag, setFlag ] = useState(false);
+  sleep(16).then(() => setFlag(true));
+
   return (<ul>{
     list && list.map((val: any, index: number) => typeof val === 'object' ? 
     <li key={index}>
       <NavLink to={'/note/' + (path && path) + val.link}>{val.folder}</NavLink>
 
       {/* 组件递归 */}
-      <NoteLabel list={val.files} path={path + val.link + '/'} />
+      {/* <NoteLabel list={val.files} path={path + val.link + '/'} /> */}
+      {flag ? <NoteLabel list={val.files} path={path + val.link + '/'} /> : null }
     </li> 
     : null)
-  }</ul>);
+  }
+  </ul>);
 }
 
 export default NoteLabel;
--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,3 +1,5 @@
+const { ['log'] : c } = console;
+
 /**
  * 截取 location.pathname 参数
  * @param src 
@@ -7,3 +9,13 @@ export const pathNameSplit = (src: string) => {
   const arr = src.split(/\//);
   return arr;
 }
+
+/**
+ * 延迟加载
+ * @param time 
+ * @returns 
+ */
+export const sleep = (time: number) => {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
